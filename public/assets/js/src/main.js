5f52464ac3885e1918e74141491ce4f480a29666
JoshSpears3 <josh.spears@sq1.com>
2016-04-11 20:27:08 -0500

    Added jquery ui easing. Work in progress hero seciton.

--- public/assets/js/src/main.js
+++ public/assets/js/src/main.js
@@ -1,4 +1,8 @@
 'use strict';
 $(function () {
-    
+    $('#navigation-arrow').on('click',function(){
+        $('html, body').animate({
+            scrollTop: $("#work").offset().top
+        }, 1800, 'easeInOutExpo');
+    });
 });
