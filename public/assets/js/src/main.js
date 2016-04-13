e0dc1fb0086f63d7c1457a27b64905f355e6ddb4
JoshSpears3 <josh.spears@sq1.com>
2016-04-13 18:19:38 -0500

    Finished building the hero section for desktop view. Also completed desktop navigation! Added parallax.js

--- public/assets/js/src/main.js
+++ public/assets/js/src/main.js
@@ -1,8 +1,37 @@
 'use strict';
 $(function () {
+
+    var nav = $('#navigation');
+    var heroSection = $('.hero-section');
+
+    $('.desktop-list').on('click', function(){
+        $('.desktop-list, .plus, .menu-item, i').toggleClass('active');
+    });
+
+    function navigationAnimation(){
+        if ($(window).scrollTop() <= $(heroSection).height() / 2 - 75) {
+            $('.desktop-list').removeClass('unextended');
+            $('.menu-item').removeClass('hoverState');
+        } else {
+            $('.desktop-list').addClass('unextended');
+            $('.menu-item').addClass('hoverState');
+        }
+    }
+
+    navigationAnimation();
+    $(window).on('scroll', navigationAnimation);
+
     $('#navigation-arrow').on('click',function(){
         $('html, body').animate({
             scrollTop: $("#work").offset().top
         }, 1800, 'easeInOutExpo');
     });
+
+    setTimeout(function(){
+        $('.hero-intro').fadeIn(1500);
+    }, 300);
+
+    setInterval(function () {
+        $('.fa-angle-down').toggleClass('wave');
+    }, 800);
 });
