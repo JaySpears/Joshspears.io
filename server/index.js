a1e5af13d78d133756e32f72521e9156049a6319
JoshSpears3 <josh.spears@sq1.com>
2018-04-02 11:30:54 -0500

    Configured assets

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ import webpack from 'webpack';
 import bodyParser from 'body-parser';
 import webpackMiddleware from 'webpack-dev-middleware';
 import webpackHotMiddleware from 'webpack-hot-middleware';
-import 'path';
+import path;
 
 // Constants.
 const config = require('../webpack.config.js');
